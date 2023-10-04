a0a1f42e1f1a52c4234c3c009d7643234a2e8b33
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-10-03 18:31:30 -0700

    removed extra css and other elements

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,26 +1,15 @@
+// This auth page is static. It will server-side
+// redirect to the app if the user is already authenticated.
+
 import React from "react";
 import { withUser, withUserTokenSSR, AuthAction } from "next-firebase-auth";
 import FirebaseAuth from "../components/FirebaseAuth";
 
-const styles = {
-	content: {
-		padding: `8px 32px`,
-	},
-	textContainer: {
-		display: "flex",
-		justifyContent: "center",
-		margin: 16,
-	},
-};
-
 const Auth = () => (
-	<div style={styles.content}>
+	<div>
 		<h3>Sign in</h3>
-		<div style={styles.textContainer}>
-			<p>
-				This auth page is <b>not</b> static. It will server-side
-				redirect to the app if the user is already authenticated.
-			</p>
+		<div>
+			<p>RateMyLoo</p>
 		</div>
 		<div>
 			<FirebaseAuth />
--- pages/loo.tsx
+++ pages/loo.tsx
@@ -1,103 +1,79 @@
-import React from "react";
-import {
-	useUser,
-	withUser,
-	withUserTokenSSR,
-	AuthAction,
-} from "next-firebase-auth";
+// This page requires is static but requires
+// authentication. Before the Firebase client SDK
+// initializes, it shows a loader. After initializing, if
+// the user is not authenticated, it client-side redirects
+// to the login page.
+//
+// Note at the bottom, there is a LoaderComponent for withUser(). A react
+// component can be used to show a loading screen or something.
+
+import React, { useCallback, useEffect, useState } from "react";
+import { useUser, withUser, AuthAction } from "next-firebase-auth";
+import Header from "../components/Header";
 import getAbsoluteURL from "../utils/getAbsoluteURL";
-import Header from "@/components/Header";
 
-const styles = {
-	content: {
-		padding: 32,
-	},
-	infoTextContainer: {
-		marginBottom: 32,
-	},
-};
+const Loo = () => {
+	const AuthUser = useUser(); // the user is guaranteed to be authenticated
 
-const Loo = ({ favoriteColor, favoriteAnimal, email }: any) => {
-	const user = useUser();
+	const [favoriteColor, setFavoriteColor] = useState();
+	const fetchData = useCallback(async () => {
+		const token = await AuthUser.getIdToken();
+		const endpoint = getAbsoluteURL("/api/example");
+		const response = await fetch(endpoint, {
+			method: "GET",
+			headers: {
+				Authorization: token,
+			},
+		});
+		const data = await response.json();
+		if (!response.ok) {
+			// eslint-disable-next-line no-console
+			console.error(
+				`Data fetching failed with status ${
+					response.status
+				}: ${JSON.stringify(data)}`
+			);
+			return null;
+		}
+		return data;
+	}, [AuthUser]);
+
+	useEffect(() => {
+		let isCancelled = false;
+		const fetchFavoriteColor = async () => {
+			const data = await fetchData();
+			if (!isCancelled) {
+				setFavoriteColor(data ? data.favoriteColor : "unknown :(");
+			}
+		};
+		fetchFavoriteColor();
+		return () => {
+			// A quick but not ideal way to avoid state updates after unmount.
+			// In your app, prefer aborting fetches:
+			// https://developers.google.com/web/updates/2017/09/abortable-fetch
+			isCancelled = true;
+		};
+	}, [fetchData]);
+
 	return (
 		<div>
-			<Header email={user.email} signOut={user.signOut} />
-			<div style={styles.content}>
-				<div style={styles.infoTextContainer}>
-					<h3>Example: SSR + data fetching</h3>
+			<Header email={AuthUser.email} signOut={AuthUser.signOut} />
+			<div>
+				<div>
+					<h3>Loo page</h3>
 					<p>
-						This page requires authentication. It will do a
-						server-side redirect (307) to the login page if the auth
-						cookies are not set.
+						This is where the users can decide whether the image
+						would work
 					</p>
 					<p>Your favorite color is: {favoriteColor}</p>
-					<p>
-						Your favorite animal is {favoriteAnimal}. Please send
-						lots of photos of {favoriteAnimal} to {email}.
-					</p>
 				</div>
 			</div>
 		</div>
 	);
 };
 
-export const getServerSideProps = withUserTokenSSR({
-	whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
-})(async ({ user, req }: any) => {
-	// Optionally, get other props.
-	// You can return anything you'd normally return from
-	// `getServerSideProps`, including redirects.
-	// https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
-	// const token = await user?.getIdToken();
-
-	// This endpoint uses an ID token.
-	// Note: you shouldn't typically fetch your own API routes from within
-	// `getServerSideProps`. This is for example purposes only.
-	// https://github.com/gladly-team/next-firebase-auth/issues/264
-	// const endpoint = getAbsoluteURL("/api/example", req);
-	// const response = await fetch(endpoint, {
-	// 	method: "GET",
-	// 	headers: {
-	// 		Authorization: token || "unauthenticated",
-	// 	},
-	// });
-	// const data = await response.json();
-	// if (!response.ok) {
-	// 	throw new Error(
-	// 		`Data fetching failed with status ${
-	// 			response.status
-	// 		}: ${JSON.stringify(data)}`
-	// 	);
-	// }
-
-	// This API endpoint uses cookies rather than an ID token.
-	const endpointTwo = getAbsoluteURL("/api/cookies", req);
-	const responseTwo = await fetch(endpointTwo, {
-		method: "GET",
-		// No Authorization header required, but we need to pass cookies.
-		headers: {
-			cookie: req.headers.cookie,
-		},
-		credentials: "include",
-	});
-	const dataTwo = await responseTwo.json();
-	if (!responseTwo.ok) {
-		throw new Error(
-			`Data fetching (using cookies) failed with status ${
-				responseTwo.status
-			}: ${JSON.stringify(dataTwo)}`
-		);
-	}
-
-	return {
-		props: {
-			// favoriteColor: data.favoriteColor,
-			favoriteAnimal: dataTwo.favoriteAnimal,
-			email: dataTwo.email,
-		},
-	};
-});
-
 export default withUser({
+	whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
 	whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+	// LoaderComponent: <react component>
 })(Loo);
