9ea36b80121b4a6824bd0301d796b8e1bcb9118a
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-10-10 20:35:06 -0700

    add back middleware

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";	
+import type { NextRequest } from "next/server";	
+
+export function middleware(request: NextRequest) {	
+	// Clone the request headers and set a new header	
+	const requestHeaders = new Headers(request.headers);	
+	requestHeaders.set(	
+		"Access-Control-Allow-Origin",	
+		process.env.NEXT_PUBLIC_URL as string	
+	);	
+	requestHeaders.set(	
+		"Access-Control-Allow-Methods",	
+		"GET, POST, PUT, DELETE, OPTIONS"	
+	);	
+	requestHeaders.set("Access-Control-Allow-Headers", "Authorization");	
+
+	// Set request headers in NextResponse.rewrite	
+	const response = NextResponse.next({	
+		request: {	
+			// New request headers	
+			headers: requestHeaders,	
+		},	
+	});	
+
+	return response;
