db83d2794612801581d96440ef0a2d428ce1456c
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-11-29 11:22:00 -0800

    wip authorized access restricted endpts

---
+++ tests/access-restricted-endpoints-authorized.spec.ts
@@ -0,0 +1,36 @@
+import { testAuth } from "./auth.fixture";
+import { expect } from "@playwright/test";
+
+testAuth(
+	"Authorized access restricted API endpoints using GET",
+	async ({ request }) => {
+		const res1 = await request.get("/api/images");
+		expect(res1.status() === 400);
+		const res2 = await request.get("/api/firestore");
+		expect(res2.status() === 400);
+
+		const res3 = await request.get("/api/images?userOnly=1");
+		expect(res3.status() === 200);
+		const res4 = await request.get("/api/images=notFromUser=1");
+		expect(res4.status() === 200);
+	}
+);
+
+testAuth(
+	"Authorized access restricted API endpoints using POST",
+	async ({ request }) => {
+		const dummy = {
+			title: "dummy",
+			body: "data",
+		};
+		const res1 = await request.post("/api/images", { data: dummy });
+		expect(res1.status() === 400);
+		const res2 = await request.post("/api/firestore", { data: dummy });
+		expect(res2.status() === 400);
+
+		// const res3 = await request.post("/api/images?userOnly=1");
+		// expect(res3.status() === 200);
+		// const res4 = await request.post("/api/images=notFromUser=1");
+		// expect(res4.status() === 200);
+	}
+);
