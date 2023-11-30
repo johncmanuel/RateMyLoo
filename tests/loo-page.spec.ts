74e2eafa4378de49b297bcfa21649dbf5bccea91
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-11-30 11:07:29 -0800

    loo page test case may be slow

--- tests/loo-page.spec.ts
+++ tests/loo-page.spec.ts
@@ -2,6 +2,7 @@ import { expect } from "@playwright/test";
 import { testAuth } from "./auth.fixture";
 
 testAuth("Check if images appear", async ({ page }) => {
+	testAuth.slow();
 	await expect(
 		page.getByRole("img", { name: "Image of bathroom picture" }).nth(0)
 	).toBeVisible();
