cb366737990a40ae64f34a9eef16247ec48156da
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-11-30 11:06:59 -0800

    loo page test case

---
+++ tests/loo-page.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from "@playwright/test";
+import { testAuth } from "./auth.fixture";
+
+testAuth("Check if images appear", async ({ page }) => {
+	await expect(
+		page.getByRole("img", { name: "Image of bathroom picture" }).nth(0)
+	).toBeVisible();
+	await expect(
+		page.getByRole("img", { name: "Image of bathroom picture" }).nth(1)
+	).toBeVisible();
+});
