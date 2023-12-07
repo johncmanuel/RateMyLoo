be15fe57efe56d8c6c324761a6ccf17190db98e7
zohairmamdani <zohairmamdani@gmail.com>
2023-12-07 10:25:09 -0800

    Added styling to h tags in index.tsx. Added styling to myButton to allow it to be centered.

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,13 +25,47 @@ const Home = () => {
 			<Header email={user.email} signOut={user.signOut} />
 			<div>
 				<div>
-					<h1>Welcome to Rate My Loo!</h1>
-					<h2>Please log in or sign up to gain access</h2>
-					<h3>What is Rate My Loo?</h3>
-					<h4>Rate My Loo is a website where you can upload</h4>
-					<h5>images of your bathroom and compare your own to others!</h5>
-					<h6>Log in and see for yourself!</h6>
-					<button type="button" onClick={handleButtonClick}>Log into FireBase</button>
+				<h1
+					//className="w-full h-100 p-10 text-center font-custom"
+					style={{
+						fontFamily: "Times New Roman",
+						color: "teal",
+						fontSize: "24px",
+					}}
+				>
+					Welcome to RateMyLoo!
+				</h1>
+					<h2
+						style={{
+							fontFamily: "Times New Roman",
+							color: "grey",
+							fontSize: "24px",
+						}}
+						>Please log in or sign up to gain access</h2>
+					<h3
+						style={{
+							fontFamily: "Times New Roman",
+							color: "teal",
+							fontSize: "24px",
+							paddingTop: "10px",
+						}}>What is Rate My Loo?</h3>
+					<h4	style={{
+						fontFamily: "Times New Roman",
+						color: "grey",
+						fontSize: "24px",
+					}}>Rate My Loo is a website where you can upload</h4>
+					<h5	style={{
+						fontFamily: "Times New Roman",
+						color: "grey",
+						fontSize: "24px",
+					}}>images of your bathroom and compare your own to others!</h5>
+					<h6	style={{
+						fontFamily: "Times New Roman",
+						color: "grey",
+						fontSize: "24px",
+					}}>Log in and see for yourself!</h6>
+					<button id="myButton" onClick={handleButtonClick}>Log into FireBase</button>
+					
 				</div>
 			</div>
 			<Footer />
