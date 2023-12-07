a50628a944d61279d382eedfd9b59677a2db4eb8
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-12-06 16:27:18 -0800

    update kyle's progress

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,32 @@
-import Image from "next/image";
-import Link from "next/link";
-
 const Footer = () => {
-  return (
-    <footer className="flex flex-col
+	return (
+		<footer
+			className="flex flex-col
     text-black-100 mt-5 border-t 
-    border-gray-300">
-        <div className="felx max-md:flex-col
+    border-gray-300"
+		>
+			<div
+				className="felx max-md:flex-col
         flex-warp justify-between gap-5 sm:px-16
-        px-6 py-10">
-            <div className="flex flex-col 
-            justify-start items-start gap-6">
-                <p className="text-base
-                text-gray-400">
-                    2 Asians & Brown Guy<br />
-                    All rights reserved &copy; 
-                </p>   
+        px-6 py-10"
+			>
+				<div
+					className="flex flex-col 
+            justify-start items-start gap-6"
+				>
+					<p
+						className="text-base
+                text-gray-400"
+					>
+						2 Asians & Brown Guy
+						<br />
+						Copyright {new Date().getFullYear()}. All rights
+						reserved. &copy;
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-            </div>
-
-        </div>
-
-    </footer>
-  )
-}
-
-export default Footer
+export default Footer;
--- pages/user.tsx
+++ pages/user.tsx
@@ -1,16 +1,13 @@
 import { useUser, withUser, AuthAction } from "next-firebase-auth";
 import Header from "../components/Header";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 import Footer from "../components/Footer";
-import '../styles/style.module.css';
+import "../styles/style.module.css";
 import { authFetch } from "@/utils/authFetch";
 import DarkModeToggler from "../components/DarkModeToggler";
-//import { toggleDarkMode } from '../components/darkModeSlice';
-//import DarkModeToggle from "../components/DarkModeToggle";
-
+import Link from "next/link";
 
 const User = () => {
 	const user = useUser();
@@ -19,11 +16,10 @@ const User = () => {
 	const [images, setImages] = useState<string[]>([]);
 	const IMAGES_LIMIT = 5; // this will be an env variable in the future
 
-
 	// See full list of file type extensions for images:
 	// https://www.iana.org/assignments/media-types/media-types.xhtml#image
 	// This will also be an env variable in the future
-	const fileExtensions = ["png", "jpg", "webp", "jpeg"]; //this is the format of the file 
+	const fileExtensions = ["png", "jpg", "webp", "jpeg"];
 
 	const mimeTypes = fileExtensions.map((extension) => `image/${extension}`);
 	const acceptedInput = mimeTypes.join(", ");
@@ -37,10 +33,6 @@ const User = () => {
 			console.error("error fetching image urls: ", error);
 		}
 	};
-
-	  
-
-	  
 
 	// Uploads only 1 image at a time
 	// TODO: May want to limit amount of pictures user can
@@ -89,11 +81,6 @@ const User = () => {
 			setImages(data);
 		}
 	};
-	// pages/index.js
-	
-	
-
-
 	// Delete an image given the name of the image.
 	// Might need to make images interactable and delete
 	// them given the name of the image
@@ -117,177 +104,133 @@ const User = () => {
 		}
 	};
 
-	
-
-	//This can be replace by others but this is will show the user information to see
-	//const UserPage = ({ userEmail }) => { //useremail might have to be replace to firebaseauth
-	//	const [displayEmail, setDisplayEmail] = useState(false);
-	//
-	//	const handleButtonClick = () => {
-	//		setDisplayEmail(true);
-	//	}
-	//}
-	//End of statement
-
 	useEffect(() => {
-
-		//const applyDarkMode = (darkMode: boolean) => {
-			//const root = window.document.documentElement;
-			//root.classList.toggle('dark', darkMode);
-		//};
 		// Load user's images upon entering page
 		const im = async () => {
 			const token = (await user.getIdToken()) as string;
 			const data = await fetchUserImagesUrls(token);
 			setImages(data);
 		};
-		im();	
+		im();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-		
-		
-	}, 
-	[]);
-	
-	
+	}, []);
+
 	return (
-		
-		
-		<div className="min-h-screen dark:bg-gray-900"  >
+		<div className="min-h-screen dark:bg-gray-900">
 			<DarkModeToggler />
-			
-				<div className="h-20">
-					<nav className="flex justify-between bordera p-8">
-						<Image src="/hero2.jpeg" alt="logo" width={165} height={500} 
-						style={{ position: 'relative', bottom: '20px', right: '30px' }} />
-						<ul className="flex space-x-8 text-teal-500">
-							<li>
-								<a href="/">Home</a>
-							</li>
-							<li>
-								<a href="/loo">Loo Page</a>
-								</li>
-							<li>
-								<a href="/user">
-									User Page
-								</a>
-							</li>
-							<li>
-								<a href="/auth">
-									Login page
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
 
-				<div style={{ borderTop: "2px solid #fff ",marginTop: 20 }}>
+			{/* TODO: add navbar here via component */}
 
-				</div>
+			{/* TODO: wrap this div elem in a component and import it here */}
+			<div style={{ borderTop: "2px solid #fff ", marginTop: 20 }} />
 
+			<Header email={user.email} signOut={user.signOut} />
 
-				<Header email={user.email} signOut={user.signOut} />
-				
-				
-
-				
-				<div
-					className="flex-1 pt-36 padding-x gradient-bg"
+			<div
+				className="flex-1 pt-36 padding-x gradient-bg"
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+					marginTop: "100px", // Adjust the margin value as needed
+				}}
+			>
+				<h1
+					className="border-4 border-black-100 w-full h-100 p-10 text-center font-custom"
 					style={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'center',
-					marginTop: '100px' // Adjust the margin value as needed
+						fontFamily: "Times New Roman",
+						color: "teal",
+						fontSize: "24px",
 					}}
 				>
-				
-					<h1 className="border-4 border-black-100 w-full h-100 p-10 text-center font-custom" style={{fontFamily: 'Times New Roman', color: 'teal', fontSize: '24px'}}>
-						This is the user page
-					</h1>
-				</div>
+					This is the user page
+				</h1>
+			</div>
 
-			
-
-			
-			
-
-			<div style={{ display: 'flex' }}>
-				<p className="m-2 p-2 rounded-lg border-2 border-blue-200 bg-blue-100 font-custom" style={{ width: '50%', marginRight: '10px', fontFamily: 'Times New Roman', color: 'teal' }}>
-				User Information :
-				{user ? (
-          			<span>
-            			<br />
-            			Email: {user.email} {/* Display the user's email */}
-          			</span>
-        		)	: (
-          			<span>No user signed in</span>
-        		)}
+			<div style={{ display: "flex" }}>
+				<p
+					className="m-2 p-2 rounded-lg border-2 border-blue-200 bg-blue-100 font-custom"
+					style={{
+						width: "50%",
+						marginRight: "10px",
+						fontFamily: "Times New Roman",
+						color: "teal",
+					}}
+				>
+					User Information :
+					{user ? (
+						<span>
+							<br />
+							Email: {user.email} {/* Display the user's email */}
+						</span>
+					) : (
+						<span>No user signed in</span>
+					)}
 				</p>
-				
-			
 
-				<p className="m-2 p-2 rounded-lg border-2 border-blue-200 bg-blue-100 font-custom" style={{ width: '50%', fontFamily: 'Times New Roman', color: 'teal' }}>
-				Previous History : <a href="/loo">Press Here</a>
+				<p
+					className="m-2 p-2 rounded-lg border-2 border-blue-200 bg-blue-100 font-custom"
+					style={{
+						width: "50%",
+						fontFamily: "Times New Roman",
+						color: "teal",
+					}}
+				>
+					Previous History : <Link href="/loo">Press Here</Link>
 				</p>
 			</div>
 
-
-			
-
-
-				{images.length >= IMAGES_LIMIT ? (
+			{images.length >= IMAGES_LIMIT ? (
+				<div>
+					you cannot upload any more images (limit is {IMAGES_LIMIT}{" "}
+					images)
+				</div>
+			) : (
+				<div
+					className="m-2 p-2 rounded-lg border-2 border-blue-200 bg-gray-200"
+					style={{ color: "teal" }}
+				>
+					<p>Upload a .png or .jpg image</p>
+					{/* See /api/images.ts in POST case. */}
+					<p>File must be less than 10 megabytes</p>
 					<div>
-						you cannot upload any more images (limit is {IMAGES_LIMIT}{" "}
-						images)
-					
+						you may upload {IMAGES_LIMIT - images.length} more
+						images
 					</div>
-				) : (
-					<div className="m-2 p-2 rounded-lg border-2 border-blue-200 bg-gray-200" style={{color: 'teal'}}>
-						<p>Upload a .png or .jpg image</p>
-						{/* See /api/images.ts in POST case. */}
-						<p>File must be less than 10 megabytes</p>
-						<div>
-							you may upload {IMAGES_LIMIT - images.length} more
-							images
-						</div>
-						<input
-							onChange={uploadImage}
-							type="file"
-							// accept="image/png, image/jpeg"
-							accept={acceptedInput}
-						/>
-					</div>
-				)}
-				{isUploaded && <p>Successfully uploaded {name}!</p>}
-				{images.map((imageURL: string, index: number) => (
-					<div key={index}>
-						<Image
-							src={imageURL}
-							alt="Picture of bathroom for current user"
-							width={200}
-							height={200}
-							priority={true}
-						/>
-						<div></div>
-						<button onClick={async () => await deleteImage(imageURL)}>
-							(click me) Delete image
-						</button>
-					</div>
-				))}
-			
+					<input
+						onChange={uploadImage}
+						type="file"
+						// accept="image/png, image/jpeg"
+						accept={acceptedInput}
+					/>
+				</div>
+			)}
+			{isUploaded && <p>Successfully uploaded {name}!</p>}
+			{images.map((imageURL: string, index: number) => (
+				<div key={index}>
+					<Image
+						src={imageURL}
+						alt="Picture of bathroom for current user"
+						width={200}
+						height={200}
+						priority={true}
+					/>
+					<div></div>
+					<button onClick={async () => await deleteImage(imageURL)}>
+						(click me) Delete image
+					</button>
+				</div>
+			))}
+
 			<Footer />
-			
+		</div>
+	);
+};
 
-		</div>	
-			
-			
-		);
-	};
+export default withUser({
+	whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+	whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(User);
 
-	export default withUser({
-		whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
-		whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
-	})(User);
-
-
-	//potneially add footer
+//potneially add footer
